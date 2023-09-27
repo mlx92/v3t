c3fb8c2b139dca92e45b612d8ae45002e961b20f
mlx <778683342@qq.com>
2023-09-27 23:18:22 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 
 <script setup lang="ts">
-import { log } from 'console';
 import { shallowReactive, isReactive } from 'vue'
 
 defineProps<{
@@ -20,30 +19,10 @@ let age:number = 24;
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <div>sadfa</div>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
